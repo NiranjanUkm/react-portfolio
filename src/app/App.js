51f27fc7cbb2c830b721122670133cc5eeea51cf
Niranjan <officialniranjan10@gmail.com>
2024-07-30 00:48:53 +0530

    full app

---
+++ src/app/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  useLocation,
+} from "react-router-dom";
+import withRouter from "../hooks/withRouter";
+import AppRoutes from "./routes";
+import Headermain from "../header";
+import "./App.css";
+
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children;
+}
+const ScrollToTop = withRouter(_ScrollToTop);
+
+export default function App() {
+
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="App" id={load ? "no-scroll" : "scroll"}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <ScrollToTop>
+          <Headermain />
+          <AppRoutes />
+        </ScrollToTop>
+      </Router>
+    </div>
+
+  );
+}
---
+++ src/content_option.js
@@ -0,0 +1,98 @@
+const logotext = "Ni.";
+const meta = {
+    title: "Niranjan",
+    description: "I’m Niranjan data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Niranjan",
+    animated: {
+        first: "an MCA Student",
+        second: "also a Full Stack Dev",
+        third: "I develop web apps",
+    },
+    description: "Dedicated and passionate individual with a drive to deliver exceptional results.",
+    your_img_url: "src\assets\images\profile.png",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "As a dedicated student with a passion for coding and problem-solving, I'm actively seeking internship opportunities to apply my technical expertise and contribute to innovative projects. I have a strong foundation in various programming languages and markup languages. My goal is to grow professionally and make a meaningful impact in the tech industry.",
+};
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Express",
+        value: 85,
+    },
+    {
+        name: "Node",
+        value: 80,
+    },
+    {
+        name: "MongoDB",
+        value: 80,
+    },
+];
+
+const services = [
+    {
+      title: "Full Stack Development",
+      description:
+        "I design and develop scalable, efficient, and user-friendly full-stack applications using a range of technologies, including React, Node.js, Express, and MongoDB. From conceptualization to deployment, I handle all aspects of the development process to deliver high-quality solutions that meet your needs.",
+    },
+    {
+      title: "Machine Learning/AI Models",
+      description:
+        "I develop and implement machine learning and AI models to solve complex problems and drive business value. With expertise in Python and various libraries, including scikit-learn and TensorFlow, I can help you build predictive models, classify data, and uncover insights that inform your decision-making.",
+    },
+    {
+      title: "AWS Cloud",
+      description:
+        "I provide expertise in designing, deploying, and managing applications on Amazon Web Services (AWS). From setting up scalable infrastructure to optimizing performance and security, I help you leverage the power of the cloud to drive innovation, reduce costs, and improve efficiency. Whether you're migrating existing workloads or building new applications, I can help you navigate the AWS ecosystem and achieve your goals.",
+    },
+  ];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "niranjanukm@gmail.com",
+    YOUR_FONE: "+91 9447277118",
+    description: "Contact me if you want to build any web app as freelance. Will be doing to for a cheepest price. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/NiranjanUkm",
+    instagram: "https://www.instagram.com/niranjan_ukm/",
+    linkedin: "https://www.linkedin.com/in/niranjan-436ab722a/",
+    twitter: "https://x.com/Niranjanukm1",
+};
+export {
+    meta,
+    dataabout,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
